81354f747767b78d032158e1c0d59141bf9f93b9
Sonali Goel <sonali.goel@tesco.com>
2026-02-09 14:06:12 +0000

    updates

--- practice/src/Day12.java
+++ practice/src/Day12.java
@@ -12,16 +12,29 @@ public class Day12 {
 
         boolean hasEven = input.stream().anyMatch(i -> i%2 == 0);
         boolean allEven = input.stream().allMatch(i -> i%2==0);
+        boolean notOdd = input.stream().noneMatch(i -> i%2!=0);
         Optional<Integer> findFirst = input.stream().filter(i -> i%2 == 0).findFirst();
         Optional<Integer> findAny = input.stream().filter(i -> i%2 ==0).findAny();
 
         System.out.println("Has any even values: " + hasEven);
         System.out.println("Has all even values: " + allEven);
+        System.out.println("Has any odd values: " +notOdd);
         System.out.println("Find first even value: " + findFirst.get());
         System.out.println("Find any even value: " + findAny.get());
 
+        List<String> words = List.of("apple", "banana", "avocado", "cherry", "date");
+        //Check if any word start with 'a'
+        boolean hasWordWithA = words.stream().anyMatch(s -> s.startsWith("a"));
+        System.out.println("Any words starts with 'a': " + hasWordWithA);
 
+        //Check if all words have length > 3
+        //use peek.
+        boolean allWordsWithLengthThree = words.stream().allMatch( s -> s.length() > 3 );
+        System.out.println("Has all words length greater than 3: " + allWordsWithLengthThree);
 
+        //Check none of the words is empty
+        boolean noEmptyWord = words.stream().noneMatch(String::isEmpty);
+        System.out.println("Has NO empty words: " +noEmptyWord);
     }
 
     static void main(String[] args) {
